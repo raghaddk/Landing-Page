5e60e0b5714b8339871adfddb2f57f0770b53102
Raghad Alarwi <raghad.alarwi1@gmail.com>
2020-06-19 13:08:29 +0300

    My initial

---
+++ README.md
@@ -0,0 +1,40 @@
+# Landing Page Project
+
+## Table of Contents
+
+* [project description](#description)
+* [Instructions](#instructions)
+* [Index.html](#index)
+* [App.js](#app)
+* [Styles.css](#style)
+* [Running](#running)
+
+
+## project description
+
+The project is one page website that Contains navigation bar which is linked to each section in the page. 
+
+
+## Instructions
+
+The starter project has some HTML and CSS styling to display a static version of the Landing Page project. You'll need to convert this project from a static project to an interactive one. This will require modifying the HTML and CSS files, but primarily the JavaScript file.
+
+To get started, open `js/app.js` and start building out the app's functionality
+
+For specific, detailed instructions, look at the project instructions in the Udacity Classroom.
+
+## Index.html
+you have an empty navigation that you should implemented with JavaScript. Also, you have three section and you should to implement new section to become four sections.
+
+## App.js
+Here is the functionally happens. you have to implement three things. First you have to build the navigation with creating unordered list.
+after that, you have to do the activation part which section is being viewed while scrolling through the page.
+the last step, the file listens to the click. it means when you click on section of navigation it will scroll you to the same section you clicked.
+
+## Styles.css
+here you have change the name of the active class if you put another one in the JS. 
+you have to change the styles to be responsive whenever it's needed.
+
+## Running
+To open the website download it by cloning the repository or downloaded it as zip and open the `index.html` file in the browser.
+
---
+++ js/app.js
@@ -0,0 +1,160 @@
+/**
+ * 
+ * Manipulating the DOM exercise.
+ * Exercise programmatically builds navigation,
+ * scrolls to anchors from navigation,
+ * and highlights section in viewport upon scrolling.
+ * 
+ * Dependencies: None
+ * 
+ * JS Version: ES2015/ES6
+ * 
+ * JS Standard: ESlint
+ * 
+*/
+
+/**
+ * Define Global Variables
+ * 
+*/
+const navbarList = document.getElementById('navbar__list');
+const sections = document.querySelectorAll('section');
+
+
+
+
+/**
+ * End Global Variables
+ * Start Helper Functions
+ * 
+*/
+
+
+
+/**
+ * End Helper Functions
+ * Begin Main Functions
+ * 
+*/
+
+// build the nav
+function buildNavbar(){
+    var element
+    for( let section of sections ){
+        console.log(section)
+        var secID   = section.id;
+        var dataNav = section.dataset.nav;
+        element += ` <li> <a class='menu__link ${secID}' href='#${secID}'>${dataNav}</a> </li> `;
+    }
+    navbarList.innerHTML = element;  
+};
+    
+
+    
+
+
+// Add class 'active' to section when near top of viewport
+
+    
+
+// function active(){
+// window.addEventListener('scroll', function () {
+
+//     let section = document.querySelectorAll('.navbar__menu a');;
+//     let position = section.getBoundingClientRect().top;
+//     sections.forEach(section => {
+// 	if(position.top >= 0 && position.bottom <= window.innerHeight) {
+//         section.classList.add('your-active-class');
+//         section.style.cssText = "background-color: red ";
+//     }else {
+//         section.classList.remove('your-active-class');
+//     }
+// });
+
+// });
+// };
+
+
+function active(){
+    var scrollpos = window.scrollY;
+
+    var section = document.querySelectorAll("section");
+
+    window.addEventListener('scroll', function(){ 
+        scrollpos = window.scrollY;
+        section.forEach( element => 
+            {
+                if(scrollpos > element.offsetTop){
+                    element.previousElementSibling.classList.remove("active")
+                    element.classList.add("active");
+
+                    var navElementclass = element.getAttribute("id");
+                    var li    = document.getElementsByClassName(navElementclass);
+                    var allLi = document.querySelectorAll("li a");
+                    
+                    allLi.forEach( a => {
+                        a.classList.remove("background")
+                    } )
+                    console.log(allLi)
+                    
+                    li[0].classList.add("background")
+
+                }
+                else if( scrollpos > ( ( element.offsetTop - element.offsetHeight ) +200 )){
+                    element.classList.remove("active");
+                }
+            }
+            
+        )
+    });
+
+
+};
+
+
+
+
+// Scroll to anchor ID using scrollTO event
+
+function scrollToSection(){
+
+    $(".navbar__menu a").on('click', function() {
+        $("html, body").animate({
+        scrollTop: $($.attr(this, 'href')).offset().top}, 850);
+    
+        });
+    
+    };
+
+
+
+window.onresize = function(){
+    var li = document.querySelectorAll("li");
+    if(window.innerWidth < 800){
+        li.forEach(  l => {
+            l.style.display = "block"
+        })
+    }else{
+        li.forEach(  l => {
+            l.style.display = "inline-block"
+        })
+    }
+}
+
+
+/**
+ * End Main Functions
+ * Begin Events
+ * 
+*/
+
+// Build menu 
+buildNavbar();
+
+// Scroll to section on link click
+scrollToSection();
+
+// Set sections as active
+// activeClass();
+
+active()
